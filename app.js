6aa2fcb9bbed78594ecb0df1335ee29905a9e2cf
AshwathSpark <ashwathspark@gmail.com>
2021-07-27 16:01:00 +0530

    intro

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs');
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use("/static", express.static('./static'));
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+const port1 = 5000;
+
+app.listen(port1, () => console.log(`Port ${port1}`));
+
