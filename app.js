9bb04876bcb9c847c06ec6a5fcc523dc768230f6
AshwathSpark <ashwathspark@gmail.com>
2021-07-30 06:45:23 +0530

    mongoose

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ const express = require('express')
 const path = require('path')
 const ejs = require('ejs');
 const app = express();
+const Coupon = require('./models/coupon')
+const mongoose = require('mongoose')
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.use("/static", express.static('./static'));
 app.use(express.urlencoded({ extended: true }));
+const dbUrl =  "mongodb://localhost:27017/coupon";
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => { console.log("Connected db") });
 app.get('/', (req, res) => {
     res.render('home');
 });
 app.get('/contact', (req, res) => {
     res.render('contact');
+});
+app.get('/buy', (req, res) => {
+    let coupon = await Coupon.find({});
+    res.render('buy',{coupon});
 });
 app.get('/about', (req, res) => {
     res.render('about');
